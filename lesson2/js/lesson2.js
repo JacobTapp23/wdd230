87ded1e29d20bc252f98b5d7b49470ce40a37557
JacobTapp23 <Jacobtapp12@byui.edu>
2022-01-15 09:01:03 -0700

    added lesson 2 webpage

---
+++ lesson2/js/lesson2.js
@@ -0,0 +1,4 @@
+let currentdate = document.lastModified;
+      document.querySelector(".modified").textContent =
+        "Last Updated: " + currentdate;
+document.querySelector(".year").innerHTML = new Date().getFullYear();
