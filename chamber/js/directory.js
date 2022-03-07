8cd93042bbe5a87caa82bc2aeade2450e5faff6c
JacobTapp23 <Jacobtapp12@byui.edu>
2022-03-07 11:21:54 -0700

    directory addresses

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -13,20 +13,23 @@ fetch('https://jacobtapp23.github.io/wdd230/chamber/js/data.json')
   function displayProphets(directory) {  // Create elements to add to the document
     let card = document.createElement('section');
     let h2 = document.createElement('h2');    // Change the textContent property of the h2 element to contain the prophet's full name
-    let dob = document.createElement('p');
-    let pob = document.createElement('p');
+    let address = document.createElement('p');
+    let phone = document.createElement('p');
+    let website = document.createElement('p');
     let img = document.createElement('img');
     h2.textContent = `${directory.name}`;
-    dob.textContent = `${directory.address}`;
-    pob.textContent = `${directory.phone}`;
+    address.textContent = `${directory.address}`;
+    phone.textContent = `${directory.phone}`;
+    website.textContent = `${directory.website}`;
     img.setAttribute('src', directory.imageurl);
     img.setAttribute('alt', `${directory.name}`);
     img.classList.add("directory");
     h2.classList.add("directory")
     // Add/append the section(card) with the h2 element
     card.appendChild(h2);
-    card.appendChild(dob);
-    card.appendChild(pob);
+    card.appendChild(address);
+    card.appendChild(phone);
+    card.appendChild(website);
     card.appendChild(img);
     // Add/append the existing HTML div with the cards class with the section(card)
     document.querySelector('div.cards').appendChild(card);
