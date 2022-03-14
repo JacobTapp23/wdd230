f54b7a876b900795207835a1cb94e5f8b8b6ab57
JacobTapp23 <Jacobtapp12@byui.edu>
2022-03-14 10:51:47 -0600

    added weather api stuffs

--- chamber/js/chamber.js
+++ chamber/js/chamber.js
@@ -23,15 +23,33 @@ if (d.getDay() == 1 || d.getDate() == 2) {
   document.querySelector(".meeting").style.display = "block";
 };
 
-
-let notapplicapble = " N/A"
-let temp = parseFloat(document.getElementById('temp').innerHTML);
-let windspeed = parseFloat(document.getElementById('speed').innerHTML);
+const apiURL = "http://api.openweathermap.org/data/2.5/forecast?id=5605242&appid=4dca22dd938418525bfde3d55fe13bc7";
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
+    let tempK = jsObject.list[0].main.temp;
+    let tempF = Math.round(((tempK - 273.15) * 1.8000 + 32.00)* 10) / 10;
+    document.querySelector("#temp").textContent = tempF;
+    document.querySelector("#speed").textContent = jsObject.list[0].wind.speed;
+    const iconsrc = `https://openweathermap.org/img/w/${jsObject.list[0].weather[0].icon}.png`;
+    document.querySelector('#weathericon').setAttribute('src', iconsrc);
+    document.querySelector('#condition').textContent = jsObject.list[0].weather[0].description;
+    let notapplicapble = " N/A"
+let temp = parseFloat(document.getElementById('temp').textContent);
+let windspeed = parseFloat(document.getElementById('speed').textContent);
 let windchill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16)));
 
-if (Math.round(temp) <= 50 && Math.round(windspeed) >= 3) {
+if (Math.round(temp) <= 50.00 && Math.round(windspeed) >= 3.00) {
   document.getElementById('chill').innerHTML = " " + windchill + "&#176;F";
 }
 else{
   document.getElementById('chill').innerHTML = notapplicapble;
 };
+  });
+
+
+
+
+
+  
