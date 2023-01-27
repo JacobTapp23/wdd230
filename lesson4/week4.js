09b29ae005eff51efa34508b1eff898f99ec5040
JacobTapp23 <jacobtapp12@byui.edu>
2023-01-27 13:28:48 -0700

    fixed css issues

--- lesson4/week4.js
+++ lesson4/week4.js
@@ -1,7 +1,13 @@
 const button = document.querySelector('.btn');
+const ham = document.querySelector('.ham');
+const nav = document.querySelector('.navigation')
 
 function show() {
-    button.classList.add('newstyle');
+    button.classList.toggle('newstyle');
 }
 
 button.addEventListener('click', show);
+ham.addEventListener('click', () => {
+    nav.classList.toggle('responsive');
+});
+
