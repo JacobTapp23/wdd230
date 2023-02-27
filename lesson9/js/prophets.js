0cf36b6f7a84b2c6173e7906f3e30cf413614c3b
JacobTapp23 <jacobtapp12@byui.edu>
2023-02-27 13:41:37 -0700

    fixed error error error

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -4,34 +4,41 @@ async function getProphetData() {
   const response = await fetch(requestURL);
   const data = await response.json();
   console.table(data.prophets);  // note that we reference the prophet array of the data object given the structure of the json file
-  const prophets = data['prophets'];
-  prophets.forEach(displayProphets);
+  displayProphets(data.prophets);
 }
 
 getProphetData();
 
-  function displayProphets(prophet) {  // Create elements to add to the document
+const displayProphets = (prophets) => {
+  const cards = document.querySelector('div.cards'); // select the output container element
+
+  prophets.forEach((prophet) => {
+    // Create elements to add to the div.cards element
     let card = document.createElement('section');
-    let h2 = document.createElement('h2');    // Change the textContent property of the h2 element to contain the prophet's full name
+    let h2 = document.createElement('h2');
+    let portrait = document.createElement('img');
+    let place = document.createElement('p');
     let dob = document.createElement('p');
-    let pob = document.createElement('p');
-    // let dod = document.createElement('p');
-    let img = document.createElement('img');
+
+    // Build the h2 content out to show the prophet's full name - finish the template string
     h2.textContent = `${prophet.name} ${prophet.lastname}`;
-    dob.textContent = `${prophet.birthdate}`;
-    pob.textContent = `${prophet.birthplace}`;
-    // dod.textContent =`${prophet.death}`;
-    img.setAttribute('src', prophet.imageurl);
-    img.setAttribute('alt', `${prophet.name} ${prophet.lastname} ${prophet.order}`);
-    // Add/append the section(card) with the h2 element
+    dob.textContent = `${prophet.birthdate}`
+    place.textContent = `${prophet.birthplace}`
+
+    // Build the image portrait by setting all the relevant attribute
+    portrait.setAttribute('src', prophet.imageurl);
+    portrait.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname}`);
+
+    // Append the section(card) with the created elements
     card.appendChild(h2);
+    card.appendChild(portrait);
+    card.appendChild(place);
     card.appendChild(dob);
-    card.appendChild(pob);
-    // card.appendChild(dod);
-    card.appendChild(img);
-    // Add/append the existing HTML div with the cards class with the section(card)
-    document.querySelector('div.cards').appendChild(card);
-  }
+
+    cards.appendChild(card);
+  } // end of forEach loop
+)}; // end of function expression
+
 
   //to clear out cards
 //   let cards = document.querySelectorAll('.cards');
